a7637b877d3607a7d42ca79dfeefc3fcaab2e1be
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-19 00:44:18 +0700

    fix: unused i var

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function main() {
     while (true) {
         for (const video of videos) {
             const { stderr } = spawnSync(`ffmpeg-webrtc -re -i ${prefix}${video} -c copy -f whip "${process.env.URL}"`);
-            i = (i + 1) % videos.length;
     
             if (stderr.toString() !== "") {
                 console.error(stderr.toString());

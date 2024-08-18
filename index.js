1a6263c025d77aa074f59c8968cb974ef1820f4b
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-19 00:43:56 +0700

    fix: wrap inside function

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import { spawnSync } from "child_process";
 import config from "./config.json" with { type: "json" };
 
-if (!process.env.URL) {
-    console.error("No WHIP endpoint provided");
-    return;
-}
-
-const { prefix, videos } = config;
-
-while (true) {
-    for (const video of videos) {
-        const { stderr } = spawnSync(`ffmpeg-webrtc -re -i ${prefix}${video} -c copy -f whip "${process.env.URL}"`);
-        i = (i + 1) % videos.length;
-
-        if (stderr.toString() !== "") {
-            console.error(stderr.toString());
-            return;
-        } 
+function main() {
+    if (!process.env.URL) {
+        console.error("No WHIP endpoint provided");
+        return;
+    }
+    
+    const { prefix, videos } = config;
+    
+    while (true) {
+        for (const video of videos) {
+            const { stderr } = spawnSync(`ffmpeg-webrtc -re -i ${prefix}${video} -c copy -f whip "${process.env.URL}"`);
+            i = (i + 1) % videos.length;
+    
+            if (stderr.toString() !== "") {
+                console.error(stderr.toString());
+                return;
+            } 
+        }
     }
 }
+
+main();

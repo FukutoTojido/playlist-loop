25e024d2f45f9eb1153111f155251987c7051d06
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-18 18:37:02 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import { exec } from "child_process";
+import util from "util";
+import config from "./config.json" with { type: "json" };
+
+const execAsync = util.promisify(exec);
+
+(async () => {
+    if (!process.env.URL) {
+        console.error("No WHIP endpoint provided");
+        return;
+    }
+
+    const { prefix, videos } = config;
+
+    try {
+        let i = 0;
+        while (true) {
+            const video = videos[i];
+            await execAsync(`ffmpeg-webrtc -stream_loop -1 -re -i ${prefix}${video} -c copy -f whip ${process.env.URL}`);
+
+            i = (i + 1) % videos.length;
+        }
+    } catch (error) {
+        console.error(error);
+        return;
+    }
+})();

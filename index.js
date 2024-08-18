6cc24782b818ae041550534c318abda33764c205
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-19 00:47:36 +0700

    fix: spawn sync args

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ function main() {
         console.error("No WHIP endpoint provided");
         return;
     }
-    
+
     const { prefix, videos } = config;
-    
+
     while (true) {
         for (const video of videos) {
-            const { stderr } = spawnSync(`ffmpeg-webrtc -re -i ${prefix}${video} -c copy -f whip "${process.env.URL}"`);
-    
+            const { stderr } = spawnSync("ffmpeg-webrtc", [`-re`, `-i`, `${prefix}${video}`, `-c`, `copy`, `-f`, `whip`, `"${process.env.URL}"`], {
+                shell: true,
+            });
+
             if (stderr) {
                 console.error(stderr.toString());
                 return;
-            } 
+            }
         }
     }
 }
